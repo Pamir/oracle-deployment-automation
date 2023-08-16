fde6a7ea45b7d73ee991ed01306ee032ec48ed1b
kohei3110 <kk.31108424@gmail.com>
2023-08-16 16:12:04 +0900

    remove extra credentials

--- terraform/bootstrap/single_instance/providers.tf
+++ terraform/bootstrap/single_instance/providers.tf
@@ -17,9 +17,4 @@ provider "azurerm" {
       delete_os_disk_on_deletion = true
     }
   }
-
-  subscription_id = "14c3937a-502f-407d-b590-bfe86895887e"
-  tenant_id       = "2dd6bfd0-1796-4d89-9a2b-974f8c239ce8"
-  client_id       = "19587688-3cec-493e-84c7-9a5f712871f5"
-  client_secret   = "qaK8Q~x.f9X7EPSo3XKv_xfrlO3Lh.kWu.Nldckf"
 }
